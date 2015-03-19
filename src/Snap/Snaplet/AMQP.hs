21f70d53527ef7b344c09b202e59e97e1f8a73fa
Joel Stanley <intractable@gmail.com>
2015-03-19 14:36:12 -0500

    Bump 1.3 - fundamentally modify snaplet init/connect logic for AMQP connection keep-alive

--- src/Snap/Snaplet/AMQP.hs
+++ src/Snap/Snaplet/AMQP.hs
@@ -7,63 +7,46 @@
 {-# LANGUAGE TypeFamilies          #-}
 
 module Snap.Snaplet.AMQP
-  ( initAMQP
-  , runAmqp
-  , mkAmqpPool
-  , AmqpState   (..)
+  ( AmqpState   (..)
   , HasAmqpPool (..)
+  , initAMQP
+  , mkAmqpPool
+  , runAmqp
   ) where
 
-import           Control.Monad.State
-import           Control.Monad.Trans.Reader
-import           Data.Configurator
-import           Data.Configurator.Types
+import           Control.Concurrent          (threadDelay)
+import           Control.Exception           (try, throw)
+import           Control.Monad               (liftM, unless, void)
+import           Control.Monad.State         (gets)
+import           Control.Monad.IO.Class      (MonadIO, liftIO)
+import           Control.Monad.Trans.Reader  (ReaderT, ask)
+import           Data.Configurator           (require)
+import           Data.IORef
 import           Data.Pool
 import           Network.AMQP
 import           Paths_snaplet_amqp
 import           Snap.Snaplet
 
--------------------------------------------------------------------------------
-type AmqpPool = Pool Channel
+--------------------------------------------------------------------------------
+type AmqpPool     = Pool Channel
+newtype AmqpState = AmqpState { amqpPoolRef :: IORef AmqpPool }
 
-newtype AmqpState = AmqpState { amqpPool :: AmqpPool }
-
--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 class MonadIO m => HasAmqpPool m where
-    getAmqpPool :: m AmqpPool
+  getAmqpPool :: m AmqpPool
 
 instance HasAmqpPool (Handler b AmqpState) where
-    getAmqpPool = gets amqpPool
+  getAmqpPool = liftIO . readIORef =<< gets amqpPoolRef
 
-instance MonadIO m => HasAmqpPool (ReaderT AmqpPool m) where
-    getAmqpPool = ask
+instance MonadIO m => HasAmqpPool (ReaderT AmqpState m) where
+  getAmqpPool = liftIO . readIORef =<< liftM amqpPoolRef ask
+
+--------------------------------------------------------------------------------
 
 -- | Initialize the AMQP Snaplet.
 initAMQP :: SnapletInit b AmqpState
 initAMQP = makeSnaplet "amqp" description datadir $ do
-    (p, c) <- mkSnapletAmqpPool
-
-    -- Cleanup
-    onUnload (destroyAllResources p)
-    onUnload (closeConnection c)
-
-    return $ AmqpState p
-
-  where
-    description = "Snaplet for AMQP library"
-    datadir = Just $ liftM (++"/resources/amqp") getDataDir
-
--------------------------------------------------------------------------------
--- | Constructs a connection in a snaplet context.
-mkSnapletAmqpPool :: (MonadIO (m b v), MonadSnaplet m) => m b v (AmqpPool, Connection)
-mkSnapletAmqpPool = do
-  conf <- getSnapletUserConfig
-  mkAmqpPool conf
-
--------------------------------------------------------------------------------
--- | Constructs a connect from Config.
-mkAmqpPool :: MonadIO m => Config -> m (AmqpPool, Connection)
-mkAmqpPool conf = do
+  conf  <- getSnapletUserConfig
   host  <- liftIO $ require conf "host"
   port  <- liftIO $ require conf "port"
   vhost <- liftIO $ require conf "vhost"
@@ -71,20 +54,72 @@ mkAmqpPool conf = do
   pass  <- liftIO $ require conf "password"
 
   let connOpts = defaultConnectionOpts
-                   { coServers = [(host, (fromInteger port))]
-                   , coVHost   = vhost
-                   , coAuth    = [plain login pass]
-                   }
+                 { coServers        = [(host, fromInteger port)]
+                 , coVHost          = vhost
+                 , coAuth           = [plain login pass]
+                 , coHeartbeatDelay = Just 1
+                 }
 
-  conn <- liftIO $ openConnection'' connOpts
-  chp  <- liftIO $ createPool (openChannel conn) closeChannel 1 30 10
+  rhalt <- liftIO $ newIORef False
+  rmc   <- liftIO $ newIORef Nothing
+  rp    <- mkAmqpPool connOpts rhalt rmc Nothing
 
-  return (chp, conn)
+  onUnload $ do
+    atomicWriteIORef rhalt True
+    maybe (return ()) closeConnection =<< readIORef rmc
 
--------------------------------------------------------------------------------
--- | Runs an AMQP action in any monad with a HasAmqpPoolonn instance.
-runAmqp :: (HasAmqpPool m) => (Channel -> IO ()) -> m ()
-runAmqp action = do
-    pool <- getAmqpPool
-    liftIO $ withResource pool $! \chan -> do
-        liftIO $! action chan
+  return $! AmqpState rp
+
+  where
+    description = "Snaplet for AMQP library"
+    datadir = Just $ liftM (++"/resources/amqp") getDataDir
+
+mkAmqpPool :: MonadIO m
+           => ConnectionOpts           -- ^ How we should connect to AMQP
+           -> IORef Bool               -- ^ Halting? (prevents reconnection)
+           -> IORef (Maybe Connection) -- ^ Connection outparam
+           -> Maybe (IORef AmqpPool)   -- ^ Active pool (lazily init, feed forward)
+           -> m (IORef AmqpPool)
+mkAmqpPool connOpts rhalt rmc mrp = liftIO $ do
+  c  <- connectAMQP
+  rp <- do
+        p <- createPool (openChannel c) closeChannel 1 30 10
+        case mrp of
+          Nothing -> newIORef p
+          Just rp -> atomicWriteIORef rp p >> return rp
+
+  addConnectionClosedHandler c True $ do
+    notice "connection closed, destroying invalidated AMQP channel pool"
+    destroyAllResources =<< readIORef rp
+    halt <- readIORef rhalt
+    unless halt $ do
+      notice "re-establishing connection"
+      void $ mkAmqpPool connOpts rhalt rmc (Just rp)
+
+  return rp
+
+  where
+    shouldRetry = maybe False (const True) mrp
+    connectAMQP = do
+      eec <- try (openConnection'' connOpts)
+      case eec of
+        Left (e :: AMQPException) -> do
+          if shouldRetry
+          then do
+               notice "failed to re-establish, trying again in 1s"
+               threadDelay 1000000
+               connectAMQP
+          else throw e
+        Right c -> do
+          atomicWriteIORef rmc (Just c)
+          notice "connection established"
+          return c
+
+notice :: String -> IO ()
+notice = putStrLn . ("snaplet-amqp: " ++)
+
+--------------------------------------------------------------------------------
+
+-- | Runs an AMQP action in any monad with a HasAmqpPool instance.
+runAmqp :: HasAmqpPool m => (Channel -> IO ()) -> m ()
+runAmqp action = getAmqpPool >>= \p -> liftIO $! withResource p $! (action $!)
